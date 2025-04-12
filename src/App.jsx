d663875d306ad6d6025cb532d5b2f06142ddc17b
mahima <mahimapatel1509@gmail.com>
2025-04-12 09:04:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import TicTacToe from '../src/components/tictactoe/Tic-Tac-Toe'
+
+function App() {
+
+  return (
+    <>
+   <TicTacToe/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/tictactoe/Tic-Tac-Toe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import circle_icon from ".."
+
+const TicTacToe = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TicTacToe;
